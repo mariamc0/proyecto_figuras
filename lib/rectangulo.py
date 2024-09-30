1c85af5d2387b6b11fd3e79edb7dd545d4b34ed6
María Mira <maria.mira.calvete@rai.usc.es>
2024-09-30 13:10:17 +0200

    Agregadas funcionalidades para el rectángulo

---
+++ lib/rectangulo.py
@@ -0,0 +1,8 @@
+def get_identificador()->str:
+    return "Rectángulo"
+
+def get_area(base:int, altura:int)->float:
+    return base*altura
+
+def get_perimetro(base:int, altura:int)->int:
+    return 2*base+2*altura
